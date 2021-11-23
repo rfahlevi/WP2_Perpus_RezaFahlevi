3079a57f235b8f252ae1257f63de95dc9c66d777
rfahlevi <rezafahlevi55@gmail.com>
2021-11-23 18:57:14 +0700

    membuat halaman login

--- application/controllers/Review.php
+++ application/controllers/Review.php
@@ -5,22 +5,31 @@ class Review extends CI_Controller
 {
     public function __construct()
     {
-        parent :: __construct();
+        parent::__construct();
         $this->load->model('Review_model');
     }
 
+
     public function index()
     {
-        $this->form_validation->set_rules('nama','Nama Pembeli',
-        'required', [
-            'required' => "Nama Pembeli harus diisi" 
-        ]);
-        $this->form_validation->set_rules('nohp','Nomor HP',
-        'required', [
-            'required' => 'Nomor HP harus diisi'
-        ]);
-        if($this->form_validation->run() == false) {
-            $data['merk'] = ['Nike','Adidas','Kickers','Eiger','Bucherri'];
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama Pembeli',
+            'required',
+            [
+                'required' => "Nama Pembeli harus diisi"
+            ]
+        );
+        $this->form_validation->set_rules(
+            'nohp',
+            'Nomor HP',
+            'required',
+            [
+                'required' => 'Nomor HP harus diisi'
+            ]
+        );
+        if ($this->form_validation->run() == false) {
+            $data['merk'] = ['Nike', 'Adidas', 'Kickers', 'Eiger', 'Bucherri'];
             $this->load->view('review/v_input', $data);
         } else {
             $data =  [
@@ -28,12 +37,10 @@ class Review extends CI_Controller
                 'nohp' => $this->input->post('nohp'),
                 'merk' => $this->input->post('merk'),
                 'ukuran' => $this->input->post('ukuran'),
-                'harga' => $this->Review_model->proses($this->input->post
-                ('merk'))
+                'harga' => $this->Review_model->proses($this->input->post('merk'))
             ];
-            
+
             $this->load->view('review/v_output', $data);
         }
-
     }
 }
